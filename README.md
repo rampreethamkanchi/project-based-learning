ae47216f9870cc523f2eedfa1732b52971fe9008
rampreethamkanchi <rampreethamkanchi@gmail.com>
2024-08-26 19:51:47 +0530

    first commit

--- README.md
+++
@@ -1,5 +0,0 @@
-im ramu
-firt step should be learn how to interact with api, so dont think like you are a builder just learn how to interact with public apis properly
-next step is to analyse how did they build them, so that it helps how to build our own
-next step is to build some real useful stuff using those apis
-next build your own apis
--- src/App (the origin of this idea).js
+++ src/App (the origin of this idea).js
@@ -1,19 +1,36 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div id="main-header">
-      Learn how to interact with apis
-    </div>
-    <div id="sub-header">
-      this is only frontend practice of nodejs, these projects will not help you build an api, but will help you to learn how to interact with existing popular apis. so these projects does not contain any backend, they may. learning from existing apis is the best way to get an idea how to build our own
-      I will be interacting with below apis, try to build sometning exiting from public apis given in this resourse <a id="prismic-blog-link" href="https://prismic.io/blog/13-best-api-project-ideas">click here</a>. 
-      In future, I want to make this open ended, so that people can add their own projects on the apis below or some other public apis they find, so that it will be helpful for people just getting started to pick any project of thier choice for thier practice
-      In future, I will add a guide on how to add your own projects in readme of this project
-    </div>
-    <div id="main-body">
-      {/* <a id="nasa" className="api"></a>
+      <div id="main-header">Learn how to interact with apis</div>
+      <div id="sub-header">
+        "Ignore this, this were my thoughts when starting this project" i'm
+        ramu firt step should be learn how to interact with api, so dont think
+        like you are a builder just learn how to interact with public apis
+        properly next step is to analyse how did they build them, so that it
+        helps how to build our own next step is to build some real useful stuff
+        using those apis next build your own apis this is only frontend practice
+        of nodejs, these projects will not help you build an api, but will help
+        you to learn how to interact with existing popular apis. so these
+        projects does not contain any backend, they may. learning from existing
+        apis is the best way to get an idea how to build our own I will be
+        interacting with below apis, try to build sometning exiting from public
+        apis given in this resourse{" "}
+        <a
+          id="prismic-blog-link"
+          href="https://prismic.io/blog/13-best-api-project-ideas"
+        >
+          click here
+        </a>
+        . In future, I want to make this open ended, so that people can add
+        their own projects on the apis below or some other public apis they
+        find, so that it will be helpful for people just getting started to pick
+        any project of thier choice for thier practice In future, I will add a
+        guide on how to add your own projects in readme of this project
+      </div>
+      <div id="main-body">
+        {/* <a id="nasa" className="api"></a>
       <a id="weather" className="api"></a>
       <a id="prismic" className="api"></a>
       <a id="covid" className="api"></a>
@@ -25,7 +42,7 @@ function App() {
       <a id="polygon" className="api"></a>
       <a id="spotify" className="api"></a>
       <a id="twitter" className="api"></a> */}
-    </div>
+      </div>
     </>
   );
 }
